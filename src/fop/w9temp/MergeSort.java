f5244b838fef895964beb915d39cea381249df98
Helmut Seidl <helmut.seidl@tum.de>
2020-11-29 22:12:26 +0100

    Exercise-Template pushed by Artemis

---
+++ src/fop/w9temp/MergeSort.java
@@ -0,0 +1,17 @@
+package fop.w9temp;
+
+import java.util.*;
+
+public class MergeSort {
+
+    /**
+     * Sortes dates with MergeSort.
+     *
+     * @param input the List of Dates to be sorted
+     */
+    public void performSort(final List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
